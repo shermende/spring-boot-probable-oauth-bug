50d6b6a0c069d55c9e5f32cbc28c7ae2d8834aae
abdys <n.u.abdysamat@gmail.com>
2020-02-26 00:07:46 +0600

    AnonymousDisableApplication

--- src/main/java/dev/shermende/anonymousdisable/AnonymousDisableApplication.java
+++ src/main/java/dev/shermende/anonymousdisable/AnonymousDisableApplication.java
@@ -2,9 +2,25 @@ package dev.shermende.anonymousdisable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class AnonymousDisableApplication {
+public class AnonymousDisableApplication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .anonymous()
+                .disable()
+                .oauth2ResourceServer()
+                .jwt()
+        ;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AnonymousDisableApplication.class, args);
