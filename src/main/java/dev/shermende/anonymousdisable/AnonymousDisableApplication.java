75a544bb0b255ee5eaec11ee5e51fd41df139f73
abdys <n.u.abdysamat@gmail.com>
2020-02-26 00:02:31 +0600

    AnonymousDisableApplication

---
+++ src/main/java/dev/shermende/anonymousdisable/AnonymousDisableApplication.java
@@ -0,0 +1,13 @@
+package dev.shermende.anonymousdisable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousDisableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousDisableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/shermende/anonymousdisable/AnonymousDisableApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.shermende.anonymousdisable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousDisableApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // empty
+    }
+
+}
